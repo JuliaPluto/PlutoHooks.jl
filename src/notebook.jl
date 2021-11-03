919294bde02ef3cd2889b1e2b636d26a2134977c
dralletje <m.c.dral@gmail.com>
2021-11-03 22:36:11 +0000

    Deploying to gh-pages from  @ efb4b6d917d7baab496206c74026b401556f4d61 🚀

--- src/notebook.jl
+++ src/notebook.jl
@@ -28,7 +28,7 @@ Let's implement some [React.js](https://reactjs.org/) features in [Pluto.jl](htt
 
 # ╔═╡ c82c8aa9-46a9-4110-88af-8638625222e3
 """
-	@use_ref(initial_value::Any)
+	@use_ref(initial_value::Any)::Ref{Any}
 
 Creates a Ref that is stable over multiple **implicit** runs of the cell. Implicit run meaning a variable (or a bond) used in this cell is updated, causing this cell to re-run. When you press shift+enter in Pluto however, this ref will reset.
 
@@ -132,7 +132,7 @@ end
 		# ... others hooks ...
 	end
 
-Experimental function to wrap a bunch of macros in a fake cell that fully refreshes when the deps provided change. This is useful if you make a macro that wraps a bunch of Pluto Hooks, and you just want to refresh the whole block when something changes. This also clears @use_ref's and @use_state's, even though these don't even have a deps argument.
+Experimental function to wrap a bunch of macros in a fake cell that fully refreshes when the deps provided change. This is useful if you make a macro that wraps a bunch of Pluto Hooks, and you just want to refresh the whole block when something changes. This also clears [`@use_ref`](@ref)'s and [`@use_state`](@ref)'s, even though these don't even have a deps argument.
 
 Not entirely sure how much this is necessary (or if I'm missing something obvious that doesn't make it necessary).
 
@@ -164,10 +164,34 @@ end
 
 # ╔═╡ 89b3f807-2e24-4454-8f4c-b2a98aee571e
 """
-Used to run a side effect only when the cell is run for the first time. This is missing the React.js functionality of specifying dependencies.
+	@use_effect(deps::Vector{Any}) do
+		# Side effect
+		return function()
+			# Cleanup
+		end
+	end
+
+Used to run a side effects that can create long running processes. A good example of this would be a HTTP server, or a task that runs an async process. Maybe it's a veeeery long running HTTP request, possibly a websocket connection to an API. 🌈
+
+Likely to be used with [`@use_state`](@ref), as without it it's kinda useless. You want to get the values you fetch in the `@use_effect` back into the notebook, and that is what `@use_state` is for.
+
+The function returned from `@use_effect` is called whenever the process is supposed to be stopped. This is either when the deps change, the cell is explicitly re-run or the cell is deleted. Make sure you write good cleanup functions! It's often seen as an afterthought, but it can make your notebook experience so much better.
+
 ```julia
-@use_effect([x, y]) do
-	x + y
+# Ping me if you have a better real world example that uses deps
+# Also don't copy this verbatim, we'll have `@use_task` that is smarter
+#   with it's cleanup and returns the task state!
+@use_effect([log_prefix])
+	task = schedule(Task() do
+		while true
+			sleep(1)
+			@info "
+		end
+	end)
+
+	return function()
+		Base.schedule(task, InterruptException(), error=true)
+	end
 end
 ```
 """
@@ -182,7 +206,6 @@ macro use_effect(f, deps)
 		cleanup_ref = $(cleanup_ref)
 
 		register_cleanup_fn = @give_me_register_cleanup_function()
-		# @info "register_cleanup_fn fn" register_cleanup_fn
 		register_cleanup_fn() do
 			$(cleanup_ref)[]()
 		end
@@ -200,11 +223,53 @@ macro use_effect(f, deps)
 	end
 end
 
+# ╔═╡ bc0e4219-a40b-46f5-adb2-f164d8a9bbdb
+"""
+	@use_memo(deps::Vector{Any}) do
+		# Expensive computation/loading
+	end
+
+Does a computation only when the deps array has changed.
+This is useful for heavy computations as well as resource fetches like file reading or fetching from the network.
+
+```julia
+# Only read a file once
+@use_memo([filename]) do
+	read(filename)
+end
+```
+
+```julia
+@use_memo([a, b]) do
+	a + b # But they're like really big numbers
+end
+```
+"""
+macro use_memo(f, deps)
+	quote
+		ref = @use_ref(nothing)
+		if @use_did_deps_change($(esc(deps)))
+			ref[] = $(esc(f))()
+		end
+		ref[]
+	end
+end
+
+# ╔═╡ 3f632c14-5f25-4426-8bff-fd315db55db5
+export @use_ref, @use_state, @use_memo, @use_effect, @use_deps
+
 # ╔═╡ c461f6da-a252-4cb4-b510-a4df5ab85065
 """
 	@use_did_deps_change(deps::Vector{Any})
 
-The most base-level `use_xxx` macro that we have, and I hope we can make it so you don't actually need this in your own code. Even [`@use_ref`](@ref) uses this!
+The most base-level `use_xxx` macro that we have, and I hope we can make it so you don't actually need this in your own code. It will, when called with deps, return `true` if the deps imply a refresh.
+
+It will always return `true` when run the first time.
+After that it will.
+
+1. `deps=nothing` will return `true`
+2. `deps=[]` will return `false`
+3. `deps=[something_else...]` will return true when the deps are different than they were before
 """
 macro use_did_deps_change(deps)
 	# Can't use @use_ref because this is used by @use_ref
@@ -247,22 +312,32 @@ macro use_did_deps_change(deps)
 	end
 end
 
-# ╔═╡ bc0e4219-a40b-46f5-adb2-f164d8a9bbdb
+# ╔═╡ 0f632b57-ea01-482b-b93e-d69f962a6d92
+md"""
+## Not really hooks but internally very hook-ish
+
+These are all for making sure you have some level of Pluto-ness active. These are made to work outside of Pluto as well, but obviously give you the opposite results :P
 """
-Does the computation only at init time.
+
+# ╔═╡ 8c2e9cad-eb63-4af5-8b52-629e8d3439bd
 """
-macro use_memo(f, deps=nothing)
-	quote
-		ref = @use_ref(nothing)
-		if @use_did_deps_change($(esc(deps)))
-			ref[] = $(esc(f))()
-		end
-		ref[]
-	end
+	is_running_in_pluto_process()
+
+This doesn't mean we're in a Pluto cell, e.g. can use @bind and hooks goodies.
+It only means PlutoRunner is available (and at a version that technically supports hooks)
+"""
+function is_running_in_pluto_process()
+	isdefined(Main, :PlutoRunner) &&
+	# Also making sure my favorite goodies are present
+	isdefined(Main.PlutoRunner, :GiveMeCellID) &&
+	isdefined(Main.PlutoRunner, :GiveMeRerunCellFunction) &&
+	isdefined(Main.PlutoRunner, :GiveMeRegisterCleanupFunction)
 end
 
-# ╔═╡ 3f632c14-5f25-4426-8bff-fd315db55db5
-export @use_ref, @use_state, @use_memo, @use_effect, @use_deps
+# ╔═╡ 84736507-7ea9-4b4b-9b70-b1e9b4b33cde
+md"""
+### Until I get the PlutoTest PR out
+"""
 
 # ╔═╡ 118991d7-f470-4775-ac44-4638f4989d58
 md"""
@@ -294,30 +369,15 @@ function Base.showerror(io::IO, expr::NotRunningInPlutoCellException)
 	print(io, "NotRunningInPlutoCell: Expected to run in a Pluto cell, but wasn't! We'll try to get these hooks to work transparently when switching from Pluto to a script.. but not yet, so just as a precaution: this error!")
 end
 
-# ╔═╡ f168c077-59c7-413b-a0ac-c0fd72781b72
-dont_be_pluto_special_value(x::Any) = x
-
-# ╔═╡ 8c2e9cad-eb63-4af5-8b52-629e8d3439bd
-"""
-	is_running_in_pluto_process()
-
-This doesn't mean we're in a Pluto cell, e.g. can use @bind and hooks goodies.
-It only means PlutoRunner is available (and at a version that technically supports hooks)
-"""
-function is_running_in_pluto_process()
-	isdefined(Main, :PlutoRunner) &&
-	# Also making sure my favorite goodies are present
-	isdefined(Main.PlutoRunner, :GiveMeCellID) &&
-	isdefined(Main.PlutoRunner, :GiveMeRerunCellFunction) &&
-	isdefined(Main.PlutoRunner, :GiveMeRegisterCleanupFunction)
-end
-
 # ╔═╡ 1b8d6be4-5ba4-42a8-9276-9ef687a8a7a3
 if is_running_in_pluto_process()
 	function dont_be_pluto_special_value(x::Main.PlutoRunner.SpecialPlutoExprValue)
 		throw(NotRunningInPlutoCellException())
 	end
 end
+
+# ╔═╡ f168c077-59c7-413b-a0ac-c0fd72781b72
+dont_be_pluto_special_value(x::Any) = x
 
 # ╔═╡ a4f9eec2-135f-433a-a19e-fd497ab34fd9
 function throw_if_not_in_pluto_for_now()
@@ -345,9 +405,25 @@ end
 	@use_is_pluto_cell()
 
 Returns whether or not this expression is running inside a Pluto cell.
-This goes further than checking if the process we're running in is started using Pluto, this actually checks if this code is part of the code that gets evaluated within a cell. 
+This goes further than checking if the process we're running in is started using Pluto, this actually checks if this code is part of the code that gets evaluated within a cell. Meant to be used directly in a Pluto cell, or returned from a macro.
 
-This is nestable, so you can use `@use_is_pluto_cell()` inside your macro and, as long as that macro is used in a Pluto cell directly, it will return true
+This is nestable, so you can use `@use_is_pluto_cell()` inside your macro own and, as long as that macro is used in a Pluto cell directly, it will return true.
+
+Using this inside a function will return whether or not that function is defined in a Pluto cell. If you then call that function from a script, it will still return true:
+
+```julia
+# 49cb409b-e564-47aa-9dae-9bc5bffa991d
+function wrong_use_of_use_is_pluto_cell()
+	return @use_is_pluto_cell()
+end
+
+# 49cb409b-e564-47aa-9dae-9bc5bffa991d
+# eval circumvents Pluto-ness
+eval(quote
+	@use_is_pluto_cell() // false
+	wrong_use_of_use_is_pluto_cell() // true
+end)
+```
 """
 macro use_is_pluto_cell()
 	# Normally you don't need this,
@@ -357,7 +433,7 @@ macro use_is_pluto_cell()
 	quote
 		if (
 			is_running_in_pluto_process() &&
-			@give_me_the_pluto_cell_id() != Main.PlutoRunner.GiveMeCellID()
+			$(Main.PlutoRunner.GiveMeCellID()) != Main.PlutoRunner.GiveMeCellID()
 		)
 			true
 		else
@@ -370,7 +446,7 @@ end
 """
 	@skip_as_script expr
 
-Only run the expression if you're running inside a pluto cell. Small wrapper around [`@use_is_pluto_cell`](@ref)
+Only run the expression if you're running inside a pluto cell. Small wrapper around [`@use_is_pluto_cell`](@ref).
 
 """
 macro skip_as_script(expr)
@@ -387,6 +463,29 @@ end
 
 # ╔═╡ 92cfc989-5862-4314-ae1b-9cbfc4b42b40
 export @skip_as_script, @use_is_pluto_cell
+
+# ╔═╡ 71963fa5-82f0-4c8d-9368-0d6ba317f59e
+# Notice that even though we run it in this cell's module,
+# that doesn't count as "being in Pluto" enough.
+@skip_as_script let
+	is_pluto_cell = eval(quote
+		@use_is_pluto_cell()
+	end)
+	if is_pluto_cell
+		error("❌ eval() thinks it is a Pluto cell! What!!")
+	else
+		md"✅ Nice, eval() is indeed not the Pluto cell"
+	end
+end
+
+# ╔═╡ 014d0172-3425-4429-b8d6-1d195bc60a66
+@skip_as_script let
+	if @use_is_pluto_cell()
+		md"✅ Nice, we are indeed running in Pluto"
+	else
+		error("❌ Uhhhhhh")
+	end
+end
 
 # ╔═╡ 3d2516f8-569e-40e4-b1dd-9f024f9266e4
 """
@@ -472,6 +571,15 @@ macro use_reducer(fn, deps=nothing)
 	end
 end
 
+# ╔═╡ e07edafd-c0fa-4fa8-b63b-1ad4d05f05bb
+@async begin
+	sleep(1)
+	1 + 1
+end
+
+# ╔═╡ c8c560bf-3ef6-492f-933e-21c898fb2db6
+md"### `@use_task`"
+
 # ╔═╡ 9ec99592-955a-41bd-935a-b34f37bb5977
 """
 Wraps a `Task` with the current cell. When the cell state is reset, sends an `InterruptException` to the underlying `Task`.
@@ -522,21 +630,28 @@ macro use_task(f, deps)
 	end
 end
 
+# ╔═╡ f8059bcb-a5bb-4c3d-a438-652b72a5be52
+md"#### `@use_task` demo"
+
 # ╔═╡ 59c673cf-3915-453a-a196-a6cd265398f0
-result = @use_task([]) do
+demo_task_that_dies_after_a_second = @skip_as_script @use_task([]) do
 	sleep(1)
 	error("hi")
 end
 
 # ╔═╡ 80269b83-bab9-4673-81d5-e75d68139969
-istaskfailed(result)
+demo_task_failed = @skip_as_script istaskfailed(demo_task_that_dies_after_a_second)
+
+# ╔═╡ 56f2ff19-c6e8-4858-8e6a-3b790fae7ecb
+md"### `@use_file(filename)`"
 
 # ╔═╡ e240b167-560c-4dd7-9801-30467d8758be
-macro use_file_change(on_update_fn, filename)
+macro use_file_change(filename)
 	quote
 		filename = $(esc(filename))
+		
 		@use_deps([filename]) do
-			file_content, set_file_content = @use_state(read(filename, String))
+			last_update_time, set_last_update_time = @use_state(time())
 	
 			@use_task([]) do
 				while true
@@ -545,7 +660,7 @@ macro use_file_change(on_update_fn, filename)
 				end
 			end
 		
-			file_content
+			last_update_time
 		end
 	end
 end
@@ -554,20 +669,41 @@ end
 macro use_file(filename)
 	quote
 		filename = $(esc(filename))
-		@use_deps([filename]) do
-			file_content, set_file_content = @use_state(read(filename, String))
-	
-			@use_task([]) do
-				while true
-					watch_file(filename)
-					set_file_content(read(filename, String))
-				end
-			end
-		
-			file_content
+		update_time = @use_file_change(filename)
+		@use_memo([update_time]) do
+			read(filename, String)
 		end
 	end
 end
+
+# ╔═╡ 8447721c-a27a-4d42-95c5-dbbc59575397
+md"""
+#### `@use_file` demo
+
+"""
+
+# ╔═╡ bfd99997-9849-482a-a7db-2d38ebb7c305
+@skip_as_script begin
+	local input = @bind demo_text_file html"""
+		<input type=text placeholder="data.csv" />
+	"""
+	md"""
+	Enter text file to read:
+	$(input)
+	"""
+end
+
+# ╔═╡ ab50b532-3d78-43cb-975a-772c87d7fa79
+@skip_as_script if ismissing(demo_text_file) || demo_text_file == ""
+	Markdown.parse("Enter a file name!")
+elseif isfile(demo_text_file)
+	demo_text_file_content = @use_file(demo_text_file)
+else
+	Markdown.parse("File `$demo_text_file` doesn't exist D:")
+end
+
+# ╔═╡ 9af74baf-6571-4a0c-b0c0-989472f18f7a
+md"### `@ingredients(julia_file_path)`"
 
 # ╔═╡ 257737f3-e3fe-45c3-b638-928b67aea027
 function pluto_file_name_to_module_name(path::String)
@@ -622,24 +758,34 @@ end
 
 # ╔═╡ 074d4029-47c4-47e9-8861-4f5885bb3cc1
 md"""
-### Ingredients Demo
+#### Ingredients Demo
 
 Enter a filename relative to where this notebook is running (could even be this file)
 and the resulting module will update every time you change the file.
 """
 
 # ╔═╡ 562f9484-fbb6-4cd6-b83c-ab7944567e2f
-@bind local_file html"""<input type=text />"""
+@skip_as_script begin
+	local input = @bind demo_ingredients_file html"""
+		<input type=text placeholder="Fun Experiment.jl" />
+	"""
+	md"""
+	Enter julia file to import:
+	$(input)
+	"""
+end
 
 # ╔═╡ ff764d7d-2c07-44bd-a675-89c9e2b00151
-if isfile("./$local_file")
-	notebook = @ingredients("./$local_file")
+@skip_as_script if ismissing(demo_ingredients_file) || demo_ingredients_file == ""
+	Markdown.parse("Enter a file name!")
+elseif isfile(demo_ingredients_file)
+	demo_notebook = @ingredients(demo_ingredients_file)
 else
-	Markdown.parse("File `./$local_file` doesn't exist D:")
+	Markdown.parse("File `$demo_ingredients_file` doesn't exist D:")
 end
 
 # ╔═╡ 19b0c6f9-999b-4804-b55b-b92dfa408912
-names(notebook, all=true)
+demo_names_from_notebook = @skip_as_script names(demo_notebook, all=true)
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
@@ -678,39 +824,51 @@ uuid = "cf7118a7-6976-5b1a-9a39-7adc72f591a4"
 # ╟─c82c8aa9-46a9-4110-88af-8638625222e3
 # ╟─1df0a586-3692-11ec-0171-0b48a4a1c4bd
 # ╟─cd048a16-37f5-455e-8b6a-c098d5f83b96
-# ╠═89b3f807-2e24-4454-8f4c-b2a98aee571e
-# ╟─c461f6da-a252-4cb4-b510-a4df5ab85065
+# ╟─89b3f807-2e24-4454-8f4c-b2a98aee571e
 # ╟─bc0e4219-a40b-46f5-adb2-f164d8a9bbdb
+# ╟─c461f6da-a252-4cb4-b510-a4df5ab85065
+# ╟─0f632b57-ea01-482b-b93e-d69f962a6d92
+# ╟─d9d14e60-0c91-4eec-ba28-82cf1ebc115f
+# ╟─cce13aec-7cf0-450c-bc93-bcc4e2a70dfe
+# ╟─8c2e9cad-eb63-4af5-8b52-629e8d3439bd
+# ╟─84736507-7ea9-4b4b-9b70-b1e9b4b33cde
+# ╟─014d0172-3425-4429-b8d6-1d195bc60a66
+# ╠═71963fa5-82f0-4c8d-9368-0d6ba317f59e
 # ╟─118991d7-f470-4775-ac44-4638f4989d58
 # ╟─39aa6082-40ca-40c3-a2c0-4b6221edda32
 # ╟─3d2516f8-569e-40e4-b1dd-9f024f9266e4
 # ╟─cf55239c-526b-48fe-933e-9e8d56161fd6
-# ╟─d9d14e60-0c91-4eec-ba28-82cf1ebc115f
-# ╟─cce13aec-7cf0-450c-bc93-bcc4e2a70dfe
 # ╟─b36e130e-578b-42cb-8e3a-763f6b97108d
 # ╠═ff97bcce-1d29-469e-a4be-5dc902676057
 # ╟─78d28d07-5912-4306-ad95-ad245797889f
 # ╟─1b8d6be4-5ba4-42a8-9276-9ef687a8a7a3
 # ╟─f168c077-59c7-413b-a0ac-c0fd72781b72
 # ╟─a4f9eec2-135f-433a-a19e-fd497ab34fd9
-# ╟─8c2e9cad-eb63-4af5-8b52-629e8d3439bd
 # ╟─9ec6b9c5-6bc1-4033-ab93-072f783184e9
 # ╟─fd653af3-be53-4ddd-b69d-3967ef6d588a
 # ╟─b25ccaf1-cf46-4eea-a4d9-16c68cf56fad
 # ╟─e5905d1e-33ec-47fb-9f98-ead82eb03be8
 # ╟─274c2be6-6075-45cf-b28a-862c8bf64bd4
-# ╠═b0350bd0-5dd2-4c73-b301-f076123144c2
 # ╟─90f051be-4384-4383-9a56-2aa584687dc3
+# ╠═e07edafd-c0fa-4fa8-b63b-1ad4d05f05bb
+# ╟─c8c560bf-3ef6-492f-933e-21c898fb2db6
+# ╟─9ec99592-955a-41bd-935a-b34f37bb5977
+# ╟─f8059bcb-a5bb-4c3d-a438-652b72a5be52
 # ╠═59c673cf-3915-453a-a196-a6cd265398f0
 # ╠═80269b83-bab9-4673-81d5-e75d68139969
-# ╟─9ec99592-955a-41bd-935a-b34f37bb5977
+# ╟─56f2ff19-c6e8-4858-8e6a-3b790fae7ecb
+# ╠═b0350bd0-5dd2-4c73-b301-f076123144c2
 # ╠═e240b167-560c-4dd7-9801-30467d8758be
 # ╠═461231e8-4958-46b9-88cb-538f9151a4b0
+# ╟─8447721c-a27a-4d42-95c5-dbbc59575397
+# ╠═bfd99997-9849-482a-a7db-2d38ebb7c305
+# ╠═ab50b532-3d78-43cb-975a-772c87d7fa79
+# ╟─9af74baf-6571-4a0c-b0c0-989472f18f7a
 # ╟─257737f3-e3fe-45c3-b638-928b67aea027
 # ╟─480dd46c-cc31-46b5-bc2d-2e1680d5c682
 # ╠═d84f47ba-7c18-4d6c-952c-c9a5748a51f8
 # ╟─074d4029-47c4-47e9-8861-4f5885bb3cc1
-# ╠═562f9484-fbb6-4cd6-b83c-ab7944567e2f
+# ╟─562f9484-fbb6-4cd6-b83c-ab7944567e2f
 # ╠═ff764d7d-2c07-44bd-a675-89c9e2b00151
 # ╠═19b0c6f9-999b-4804-b55b-b92dfa408912
 # ╟─00000000-0000-0000-0000-000000000001

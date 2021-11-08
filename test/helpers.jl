d16be2b319babb2c9a67ff0efd8c83a6660f5fd0
Pangoraw <naydex.mc+github@gmail.com>
2021-11-08 23:15:09 +0000

    Deploying to gh-pages from  @ 533d2e7e1c372b034e11a9bda4edc916b7b998fd 🚀

--- test/helpers.jl
+++ test/helpers.jl
@@ -1,3 +1,17 @@
+test_env = mktempdir()
+
+function with_test_env()
+    hooks_path = joinpath(@__DIR__, "..") |> normpath
+
+    """
+    begin
+        import Pkg
+        Pkg.activate("$test_env")
+        Pkg.develop(path="$hooks_path")
+    end
+    """
+end
+
 function noerror(cell)
     errored = cell.errored
     if errored

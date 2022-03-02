5df5336b5da39a5c6e9a2f31d94971cf81128839
Pangoraw <naydex.mc+github@gmail.com>
2022-03-02 20:42:18 +0000

    Deploying to gh-pages from  @ ecc88b9d30e10fa3df087c5cb39a4c115f2b70db 🚀

---
+++ test/without_pluto.jl
@@ -0,0 +1,35 @@
+@testset "Without Pluto" begin
+    using PlutoHooks
+
+    ref = @use_ref(1)
+    @test ref[] == 1
+
+    x = 2
+    @use_effect([x]) do
+        ref[] = x
+        () -> (ref[] = 9999)
+    end
+    @test ref[] == 2
+    # cleanup never called without pluto ✓
+    @test ref[] != 9999
+
+
+    state, setstate = @use_state(5)
+    @test state == 5
+    @test_nowarn setstate(99)
+    # setstate does nothing without pluto ✓
+    @test state == 5
+
+    y = 7
+    result = @use_deps([y]) do
+        ref2 = @use_ref(1)
+        ref2[] = y
+    end
+    @test result == 7
+
+    result = @use_memo([]) do
+      result * y
+    end
+
+    @test result == 49
+end
